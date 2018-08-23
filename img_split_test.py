5a4a1f6a1a4526c5df77aa3a666cd148fda177a5
yangxu <yangxu_qzhit@outlook.com>
2018-08-23 13:10:02 +0800

    update

---
+++ img_split_test.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+import cv2
+import glob
+import numpy as np 
+
+def img_split(img):
+    '''
+    函数说明：分割图片
+
+    Parameters:
+        img - 输入图片
+    Returns:
+        image_list - 分割后图片列表(head, rear, left, right)
+    Author:
+        yangxu
+    Modify:
+        2018-08-23
+    '''
+    image_list = []
+    w = int(img.shape[0]/2)
+    h = int(img.shape[1]/2)
+
+    image_head = img[0:w, 0:h]
+    image_rear = img[w:2*w, 0:h]
+    image_left = img[w:2*w, h:2*h]
+    image_right = img[0:w, h:2*h]
+
+    image_list.append(image_head)
+    image_list.append(image_rear)
+    image_list.append(image_left)
+    image_list.append(image_right)
+
+    return image_list
+
+
+if __name__ == '__main__':
+    images = glob.glob('.\\img_test\\*\\*.png')
+    for i in range(len(images)):
+        fname = images[i]
+        img = cv2.imread(fname)
+        image_list = img_split(img)
+
+        cv2.imwrite('.\\split\\head\\%d.png' % i, image_list[0])
+        cv2.imwrite('.\\split\\rear\\%d.png' % i, image_list[1])
+        cv2.imwrite('.\\split\\left\\%d.png' % i, image_list[2])
+        cv2.imwrite('.\\split\\right\\%d.png' % i, image_list[3])
+        del image_list[:]
+
+
+
+
+
+
+
